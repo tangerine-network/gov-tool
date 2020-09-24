261ecc9368877af261b99229a669e14f636343b2
Wei-Ning Huang <aitjcize@gmail.com>
2020-09-24 23:20:57 +0800

    Add unstake and withdraw command

--- main.go
+++ main.go
@@ -20,6 +20,23 @@ const rpcURL = "https://mainnet-rpc.tangerine-network.io"
 var governanceAddress = common.HexToAddress("0x246FcDE58581e2754f215A523C0718C4BFc8041F")
 var minStake = new(big.Int).Mul(big.NewInt(1e6), big.NewInt(1e18))
 
+var conn *ethclient.Client
+var gov *Governance
+
+func init() {
+	var err error
+
+	conn, err = ethclient.Dial(rpcURL)
+	if err != nil {
+		panic(err)
+	}
+
+	gov, err = NewGovernance(governanceAddress, conn)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func registerCmd(c *cli.Context) error {
 	if c.NArg() != 5 {
 		return fmt.Errorf("Arguments: node.key Name Email Location Url")
@@ -28,16 +45,6 @@ func registerCmd(c *cli.Context) error {
 	args := c.Args()
 
 	privKey, err := crypto.LoadECDSA(args.Get(0))
-	if err != nil {
-		return err
-	}
-
-	conn, err := ethclient.Dial(rpcURL)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-
-	gov, err := NewGovernance(governanceAddress, conn)
 	if err != nil {
 		return err
 	}
@@ -59,7 +66,7 @@ func registerCmd(c *cli.Context) error {
 	return nil
 }
 
-func payeFineCmd(c *cli.Context) error {
+func payFineCmd(c *cli.Context) error {
 	if c.NArg() != 3 {
 		return fmt.Errorf("Arguments: node.key NodeAddress Amount")
 	}
@@ -71,18 +78,7 @@ func payeFineCmd(c *cli.Context) error {
 		return err
 	}
 
-	conn, err := ethclient.Dial(rpcURL)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-
-	gov, err := NewGovernance(governanceAddress, conn)
-	if err != nil {
-		return err
-	}
-
 	nodeAddr := common.HexToAddress(args.Get(1))
-	fmt.Println(nodeAddr.Hex())
 
 	amount, err := strconv.ParseInt(args.Get(2), 10, 64)
 	if err != nil {
@@ -105,6 +101,71 @@ func payeFineCmd(c *cli.Context) error {
 	return nil
 }
 
+func unstakeCmd(c *cli.Context) error {
+	if c.NArg() < 1 {
+		return fmt.Errorf("Arguments: node.key [Amount]")
+	}
+
+	args := c.Args()
+
+	privKey, err := crypto.LoadECDSA(args.Get(0))
+	if err != nil {
+		return err
+	}
+
+	var amount *big.Int
+
+	if args.Len() == 1 {
+		amount = minStake
+	} else {
+		amountInt, err := strconv.ParseInt(args.Get(1), 10, 64)
+		if err != nil {
+			return err
+		}
+		amount = new(big.Int).Mul(big.NewInt(amountInt), big.NewInt(1e18))
+	}
+
+	auth := bind.NewKeyedTransactor(privKey)
+	tx, err := gov.Unstake(&bind.TransactOpts{
+		From:   auth.From,
+		Signer: auth.Signer,
+	}, amount)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("TxHash:", tx.Hash().String())
+
+	return nil
+}
+
+func withdrawCmd(c *cli.Context) error {
+	if c.NArg() != 1 {
+		return fmt.Errorf("Arguments: node.key")
+	}
+
+	args := c.Args()
+
+	privKey, err := crypto.LoadECDSA(args.Get(0))
+	if err != nil {
+		return err
+	}
+
+	auth := bind.NewKeyedTransactor(privKey)
+	tx, err := gov.Withdraw(&bind.TransactOpts{
+		From:   auth.From,
+		Signer: auth.Signer,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("TxHash:", tx.Hash().String())
+	return nil
+}
+
 func main() {
 	app := &cli.App{
 		Commands: []*cli.Command{
@@ -116,7 +177,17 @@ func main() {
 			{
 				Name:   "payfine",
 				Usage:  "node.key NodeAddress Amount",
-				Action: payeFineCmd,
+				Action: payFineCmd,
+			},
+			{
+				Name:   "unstake",
+				Usage:  "node.key Amount",
+				Action: unstakeCmd,
+			},
+			{
+				Name:   "withdraw",
+				Usage:  "node.key",
+				Action: withdrawCmd,
 			},
 		},
 	}
